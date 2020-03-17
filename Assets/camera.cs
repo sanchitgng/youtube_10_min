0297c02bcce8eb884632cdac6009033cfa1b2e4e
Sanchit <sanchit.sharma99@gmail.com>
2020-03-17 13:07:23 +0530

    Complete project upload.

---
+++ Assets/camera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    Rigidbody rb;
+    float speed = 100;
+    float moveHori;
+    float moveVer;
+    AudioSource audio;
+    public GameObject particleFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveHori = Input.GetAxis("Horizontal");
+        moveVer = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHori, 0, moveVer);
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickables"))
+        {
+            other.gameObject.SetActive(false);
+            audio.Play();
+            Instantiate(particleFX, transform.position, Quaternion.identity);
+
+        }
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public GameObject playerbb;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - playerbb.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerbb.transform.position + offset;
+    }
+}
